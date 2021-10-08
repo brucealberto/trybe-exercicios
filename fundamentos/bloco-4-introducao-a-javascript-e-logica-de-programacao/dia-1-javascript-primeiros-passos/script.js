b6a3f9f9bcb42533d8b9736b70b8fa7f1bd26fa0
Bruce Alberto <brucealberto2@gmail.com>
2021-10-08 18:09:02 -0300

    Exercicio 02

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,9 +1,21 @@
 // Exercicio 01
-const a = 10;
+
+/*const a = 10;
 const b = 5;
 
-console.log("Adição",a + b);
-console.log("Subtração",a - b);
-console.log("Multiplicação",a * b);
-console.log("Divisão",a / b);
-console.log("Módulo",a % b);
+console.log("Adição", a + b);
+console.log("Subtração", a - b);
+console.log("Multiplicação", a * b);
+console.log("Divisão", a / b);
+console.log("Módulo", a % b);*/
+
+// Exercicio 02
+
+// const num1 = 8;
+// const num2 = 6;
+
+// if (num1 > num2) {
+//   console.log ("O maior número é ",num1);
+// } else {
+//   console.log(num2," é o maior número");
+// }
