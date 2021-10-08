a266a8399769f2b630ad85d293ce0959cba082f6
Bruce Alberto <brucealberto2@gmail.com>
2021-10-08 18:27:06 -0300

    Exercicio 03

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -19,3 +19,17 @@ console.log("Módulo", a % b);*/
 // } else {
 //   console.log(num2," é o maior número");
 // }
+
+// Exercicio 03
+
+/*const num1 = 8;
+const num2 = 6;
+const num3 = 4;
+
+if (num1 > num2 && num1 > num3) {
+  console.log("O maior número é",num1,"que é váriavel num1");
+} else if (num2 > num1 && num2 > num3){
+  console.log("O maior número é",num2,"que é váriavel num2");
+} else {
+  console.log("O maior número é",num3,"que é váriavel num3");
+}*/
