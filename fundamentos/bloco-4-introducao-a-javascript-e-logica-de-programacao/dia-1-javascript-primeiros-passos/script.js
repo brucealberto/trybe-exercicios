c171a04be9b6e301094b3e36a5ca01a3a19dba1d
Bruce Alberto <brucealberto2@gmail.com>
2021-10-09 23:37:05 -0300

    Exercicio 11 com ajuda do gabarito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -153,3 +153,36 @@ if (custoProduto >= 0 && valorVenda >= 0) {
 } else {
   console.log("Número inválido")
 }*/
+
+// Exercicio 11
+
+let aliqINSS;
+let aliqIR;
+
+let salarioBruto = 3000.00;
+
+if (salarioBruto <= 1556.94) {
+  aliqINSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  aliqINSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  aliqINSS = salarioBruto * 0.11;
+} else {
+  aliqINSS = 570.88
+}
+
+let salarioBase = salarioBruto - aliqINSS;
+
+if (salarioBase <= 1903.98) {
+  aliqIR = 0;
+} else if (salarioBase <= 2826.65) {
+  aliqIR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+  aliqIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+  aliqIR = (salarioBase * 0.225) - 636.13;
+} else {
+  aliqIR = (salarioBase * 0.275) - 869.36;
+}
+let salarioLiq = salarioBase - aliqIR;
+console.log("O salário Liquido é",salarioLiq); 
