50fda5d767ee92f35f2ff69e4e18ada9844db2dc
Bruce Alberto <brucealberto2@gmail.com>
2021-10-13 10:52:05 -0300

    Exercicio 04

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -19,6 +19,18 @@ console.log(soma)*/
 
 // Exercicio 03
 
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media = 0;
+let soma = 0;
+
+for (let numero = 0; numero < numbers.length; numero +=1) {
+  soma += numbers[numero];
+  media = soma / numbers.length;
+}
+console.log('A média dos valores é: ' + media);*/
+
+// Exercicio 04
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 let soma = 0;
@@ -27,4 +39,9 @@ for (let numero = 0; numero < numbers.length; numero +=1) {
   soma += numbers[numero];
   media = soma / numbers.length;
 }
-console.log('A média dos valores é: ' + media);
+
+if (media > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
