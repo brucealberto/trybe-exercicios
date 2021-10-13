a407a4464a4f563afef796ebc554fe3111fafe6e
Bruce Alberto <brucealberto2@gmail.com>
2021-10-13 10:34:04 -0300

    Exercicio 02

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,7 +1,18 @@
 // Exercicio 01
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let numero = 0; numero < numbers.length; numero +=1){
   console.log(numbers[numero]);
+}*/
+
+// Exercicio 02
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let numero = 0; numero < numbers.length; numero +=1){
+  soma += numbers[numero];
 }
+
+console.log(soma)
