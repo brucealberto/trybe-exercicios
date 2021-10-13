a0a8caaa40705de19bfad60b2cd7a583cead2926
Bruce Alberto <brucealberto2@gmail.com>
2021-10-13 15:54:59 -0300

    Exercicio 05 com resoluçao do gabarito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -31,7 +31,7 @@ console.log('A média dos valores é: ' + media);*/
 
 // Exercicio 04
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 let soma = 0;
 
@@ -44,4 +44,17 @@ if (media > 20) {
   console.log("valor maior que 20");
 } else {
   console.log("valor menor ou igual a 20");
+}*/
+
+// Exercicio 05
+
+/*let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = numbers[0];
+for (let numero = 0; numero < numbers.length; numero += 1) {
+  if(numbers[numero] > maiorNumero){
+    maiorNumero = numbers[numero];
+  }
+
 }
+
+console.log(maiorNumero)*/
