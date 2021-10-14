4a168de8ceeb3969d36029b2ef1187cb14531f61
Bruce Alberto <brucealberto2@gmail.com>
2021-10-13 23:38:08 -0300

    Exercicio 09 consegui fazer

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -107,4 +107,4 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let numero = 0; numero < numbers.length; numero += 1) {
   divisao = numbers[numero] / 2;
   console.log(divisao);
-}*/
+}*/ 
