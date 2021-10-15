43c1251f88ca70156b3051fd92eae8c0fb404d81
Bruce Alberto <brucealberto2@gmail.com>
2021-10-14 22:42:53 -0300

    Exercicio 03 retirado do gabarito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -22,3 +22,23 @@ for (let index = 1; index <= n; index += 1) {
   }
   console.log(aster)
 }*/
+
+// Exercico 03
+
+/*let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1; 
+};*/ 
