5f8eacf870bdf8cf87a0584bfcf4abc4c04b5ba9
Bruce Alberto <brucealberto2@gmail.com>
2021-10-14 22:16:03 -0300

    Exercicio 01

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,12 @@
+// Exercicio 01
+
+/*let n = 5;
+let aster = '';
+for (let index = 0; index < n; index += 1) {
+  aster += '*'
+}
+
+for (let index = 0; index < n; index += 1) {
+  console.log(aster)
+}*/
+
