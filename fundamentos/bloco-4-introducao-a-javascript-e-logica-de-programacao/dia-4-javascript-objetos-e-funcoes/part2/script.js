ed0bba7e8b1c2076149bfc40deb05a39206155df
Bruce Alberto <brucealberto2@gmail.com>
2021-10-16 20:34:37 -0300

    Exercicio 02 com ajuda do gabarito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/part2/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/part2/script.js
@@ -1,6 +1,6 @@
 // Exercicio 01
 
-function palindrome(word) {
+/*function palindrome(word) {
 
 
   if (word[0] == word[word.length - 1]) {
@@ -11,4 +11,18 @@ function palindrome(word) {
   return word;
 }
 
-console.log(palindrome('ovo'));
+console.log(palindrome('ovo'));*/
+
+// Exercicio 02
+
+function recebeArray(valueArray) {
+  let indiceMaior = 0;
+  for (let key in valueArray) {
+    if (valueArray[indiceMaior] < valueArray[key]) {
+      indiceMaior = key;
+    }
+  }
+  return indiceMaior;
+  //indexOf
+}
+console.log(recebeArray([2, 3, 6, 7, 10, 1]));
