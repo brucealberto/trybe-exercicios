6712e45769b78a2abca42549f00068ba3989101f
Bruce Alberto <brucealberto2@gmail.com>
2021-10-22 23:55:06 -0300

    ainda não terminei

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -23,38 +23,49 @@ const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
 // let container = document.querySelector('#days');
 // container.appendChild(dias)
 // dias.innerHTML = 'teste';
-function countDays(){
-for (let index = 0; index < dezDaysList.length; index += 1) {
-  //dezDaysList[index] += dias
-  let dias = document.createElement('li');
-  dias.className = 'day';
-  let container = document.querySelector('#days');
-  container.appendChild(dias)
-  dias.innerHTML = dezDaysList[index];
+function countDays() {
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    //dezDaysList[index] += dias
+    let dias = document.createElement('li');
+    dias.className = 'day';
+    let container = document.querySelector('#days');
+    container.appendChild(dias)
+    dias.innerHTML = dezDaysList[index];
 
-  if (dezDaysList[index] == 24 || dezDaysList[index] == 25 || dezDaysList[index] == 31) {
-    let dias = document.createElement('li');
-    dias.className = 'day holyday';
-    
-    // console.log(dias)
-  } 
-   if (dezDaysList[index] == 4 || dezDaysList[index] == 11 || dezDaysList[index] == 18 || 
-    dezDaysList[index] == 25) {
-    let dias = document.createElement('li');
-    dias.className = 'day friday';
-    
-    // console.log(dias)
+    if (dezDaysList[index] == 24 || dezDaysList[index] == 25 || dezDaysList[index] == 31) {
+      let dias = document.createElement('li');
+      dias.className = 'day holiday';
+
+      // console.log(dias)
+    }
+    if (dezDaysList[index] == 4 || dezDaysList[index] == 11 || dezDaysList[index] == 18 ||
+      dezDaysList[index] == 25) {
+      let dias = document.createElement('li');
+      dias.className = 'day friday';
+
+      // console.log(dias)
+    }
   }
-}
 }
 countDays();
 
 // Exercicio 02
- function botaoFeriado(Feriados){
-let criarBotao = document.createElement('button');
-criarBotao.id = 'btn-holiday';
-criarBotao.innerHTML = Feriados
- let botaoPai = document.querySelector('.buttons-container');
-botaoPai.appendChild(criarBotao)
- }
-botaoFeriado('Sexta-feira')
+function botaoFeriado(Feriados) {
+  let criarBotao = document.createElement('button');
+  criarBotao.id = 'btn-holiday';
+  criarBotao.innerHTML = Feriados
+  let botaoPai = document.querySelector('.buttons-container');
+  botaoPai.appendChild(criarBotao)
+  criarBotao.addEventListener('click',eventoClick)
+  
+  // criarBotao.addEventListener('click', eventoClick)
+}
+botaoFeriado('Feriados');
+
+// Exercicio 03
+
+function eventoClick() {
+  let eventoDia = document.getElementsByClassName('day holiday');
+  eventoDia.style.backgroundColor = 'blue';
+}
+console.log(document.querySelector('.day'))
