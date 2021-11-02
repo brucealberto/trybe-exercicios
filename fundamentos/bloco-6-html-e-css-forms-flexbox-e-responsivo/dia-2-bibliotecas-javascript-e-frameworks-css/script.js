57456d691772e0630709c8eb445ba50c66eae3b9
brucealberto <brucealberto2@gmail.com.br>
2021-11-01 23:53:26 -0300

    teste2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,51 @@
+const estados = [
+    "AC - Acre",
+    "AL - Alagoas",
+    "AP - Amapá",
+    "AM - Amazonas",
+    "BA - Bahia",
+    "CE - Ceará",
+    "DF - Distrito Federal",
+    "ES - Espírito Santo",
+    "GO - Goías",
+    "MA - Maranhão",
+    "MT - Mato Grosso",
+    "MS - Mato Grosso do Sul",
+    "MG - Minas Gerais",
+    "PA - Pará",
+    "PB - Paraíba",
+    "PR - Paraná",
+    "PE - Pernambuco",
+    "PI - Piauí",
+    "RJ - Rio de Janeiro",
+    "RN - Rio Grande do Norte",
+    "RS - Rio Grande do Sul",
+    "RO - Rondônia",
+    "RR - Roraíma",
+    "SC - Santa Catarina",
+    "SP - São Paulo",
+    "SE - Sergipe",
+    "TO - Tocantins",
+  ];
+  
+  const campoSelect = document.querySelector("#UF");
+  
+  for (let index = 0; index < estados.length; index += 1) {
+    let option = document.createElement("option");
+    option.className = "estados";
+    campoSelect.appendChild(option);
+  
+    option.innerHTML = estados[index];
+  }
+  
+  const botaoEnviar = document.querySelector("#btn-enviar");
+  botaoEnviar.addEventListener("click", (event) => {
+    event.preventDefault;
+  });
+  
+  
+  
+  
+  
+  
+  
