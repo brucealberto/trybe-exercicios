d0882f860fa5b4a9d798df1aad30689a08e681e7
brucealberto <brucealberto2@gmail.com.br>
2021-11-20 10:44:27 -0300

    Exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,15 +1,24 @@
-const pessoaContratada = (nomeCompleto) => {
-  const email = nomeCompleto.toLowerCase().split(' ').join('_'); // split separa as palavras, join junta elas com _
-  return  { nomeCompleto, email: email + '@trybe.com' };
+// Exercicio 1
+// const pessoaContratada = (nomeCompleto) => {
+//   const email = nomeCompleto.toLowerCase().split(' ').join('_'); // split separa as palavras, join junta elas com _
+//   return  { nomeCompleto, email: email + '@trybe.com' };
 
-};
+// };
 
-const newEmployees = (nomeCompleto) => {
-  const employees = {
-    id1: pessoaContratada('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: pessoaContratada('Luiza Drumon'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: pessoaContratada('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-  };
-  return employees;
-};
-console.log(newEmployees(pessoaContratada))
+// const newEmployees = (nomeCompleto) => {
+//   const employees = {
+//     id1: pessoaContratada('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+//     id2: pessoaContratada('Luiza Drumon'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+//     id3: pessoaContratada('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+//   };
+//   return employees;
+// };
+// console.log(newEmployees(pessoaContratada))
+// Exercicio 2  
+const checaNumeros = (meuNumero, numero) => meuNumero === numero;
+
+const resultadoSorteio = (meuNumero, callback) => {
+  const numero = Math.floor(Math.random() * 5) + 1;
+  return callback(meuNumero, numero) ? 'Tente Novamente' : 'Parabéns você Ganhou!';
+}
+console.log(resultadoSorteio(3, checaNumeros));
