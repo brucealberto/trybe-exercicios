3ed384f755770dc441d43022d3d9e10800bb7025
brucealberto <brucealberto2@gmail.com.br>
2021-11-21 20:00:16 -0300

    Exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise.js
@@ -65,18 +65,58 @@ const books = [
 
 //1 - Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 //Dica: Use a função map
-const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-];
+// const expectedResult = [
+//   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+//   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+//   'Fundação - Ficção Científica - Isaac Asimov',
+//   'Duna - Ficção Científica - Frank Herbert',
+//   'A Coisa - Terror - Stephen King',
+//   'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+// ];
 
-function formatedBookNames() {
-  // escreva seu código aqui
-  return books.map((book) =>`${book.name} - ${book.genre} - ${book.author.name}`
-    )
+// function formatedBookNames() {
+//   // escreva seu código aqui
+//   return books.map((book) =>`${book.name} - ${book.genre} - ${book.author.name}`
+//     )
+//   }
+//   console.log(formatedBookNames());
+
+  //2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+  //Dica: use as funções map , sort
+  const expectedResult = [
+    {
+      age: 31,
+      author: 'Isaac Asimov',
+    },
+    {
+      age: 38,
+      author: 'H. P. Lovecraft',
+    },
+    {
+      age: 39,
+      author: 'Stephen King',
+    },
+    {
+      age: 43,
+      author: 'George R. R. Martin',
+    },
+    {
+      age: 45,
+      author: 'Frank Herbert',
+    },
+    {
+      age: 62,
+      author: 'J. R. R. Tolkien',
+    },
+  ];
+  
+  function nameAndAge() {
+    // escreva seu código aqui
+    return books.map((book) => (
+    {
+      age: book.releaseYear - book.author.birthYear,
+      author: book.author.name,
+    }
+    )).sort((age1, age2) => age1.age - age2.age)
   }
-  console.log(formatedBookNames());
+  console.log(nameAndAge())
