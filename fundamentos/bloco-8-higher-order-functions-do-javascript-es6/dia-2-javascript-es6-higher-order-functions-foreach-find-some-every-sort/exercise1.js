ca6b7bcf9d9a51c3012d3a7edf9db9dd030be18a
brucealberto <brucealberto2@gmail.com.br>
2021-11-21 09:54:43 -0300

    Exercicio 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -105,53 +105,64 @@ const books = [
 // console.log(getNamedBook());
 
 // 4 - Ordene os livros por data de lançamento em ordem decrescente.
-const expectedResult = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: { name: 'George R. R. Martin', birthYear: 1948 },
-    releaseYear: 1991,
-  },
-  {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: { name: 'Stephen King', birthYear: 1947 },
-    releaseYear: 1986,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: { name: 'Frank Herbert', birthYear: 1920 },
-    releaseYear: 1965,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951,
-  },
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928,
-  },
-];
+// const expectedResult = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: { name: 'George R. R. Martin', birthYear: 1948 },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: { name: 'Stephen King', birthYear: 1947 },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Frank Herbert', birthYear: 1920 },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+// ];
 
-function booksOrderedByReleaseYearDesc() {
-  // escreva aqui seu código
-  return books.sort((bookA, bookB) => bookB.releaseYear - bookA.releaseYear)
+// function booksOrderedByReleaseYearDesc() {
+//   // escreva aqui seu código
+//   return books.sort((bookA, bookB) => bookB.releaseYear - bookA.releaseYear)
+// }
+// console.log(booksOrderedByReleaseYearDesc());
+
+// 5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário.
+const expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+  return books.every((book) =>{
+    book.author.birthYear > 1900 && book.author.birthYear <= 2000
+  })
 }
-console.log(booksOrderedByReleaseYearDesc());
+console.log(everyoneWasBornOnSecXX());
