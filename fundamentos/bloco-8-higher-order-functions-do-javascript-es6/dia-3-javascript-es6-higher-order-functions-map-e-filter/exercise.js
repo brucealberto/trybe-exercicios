33d1636ad7d3c630b3e05e571c346a780216cc39
brucealberto <brucealberto2@gmail.com.br>
2021-11-21 21:33:18 -0300

    Exercicio 4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise.js
@@ -162,33 +162,33 @@ const books = [
 
 // 4 - Crie um array ordenado pelos livros com mais de 60 anos de publicação e ordene-o pelo livro mais velho.
 // Dica: use as funções filter e sort
-const expectedResult = [
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954,
-  },
-];
+// const expectedResult = [
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+// ];
 
-function oldBooksOrdered() {
-  // escreva seu código aqui
-  const currentYear = new Date().getFullYear()
-  return books.filter((book) => book.releaseYear < currentYear - 60).sort((book1, book2) => book1.releaseYear - book2.releaseYear)
-}
-console.log(oldBooksOrdered());
+// function oldBooksOrdered() {
+//   // escreva seu código aqui
+//   const currentYear = new Date().getFullYear()
+//   return books.filter((book) => book.releaseYear < currentYear - 60).sort((book1, book2) => book1.releaseYear - book2.releaseYear)
+// }
+// console.log(oldBooksOrdered());
