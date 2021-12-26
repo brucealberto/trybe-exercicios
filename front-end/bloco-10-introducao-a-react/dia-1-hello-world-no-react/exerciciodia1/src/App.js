efe43d722bee3b121d8fda9e32568e7f0e208101
bruce-alberto <brucealberto2@gmail.com>
2021-12-26 10:10:49 -0300

    Exercicio 3

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exerciciodia1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exerciciodia1/src/App.js
@@ -1,19 +1,32 @@
 
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
-const Task = (value) => {
+// const Task = (value) => {
+//   return (
+//     <li key={value}>{value}</li>
+//   );
+// }
+
+// const tarefas = ['Acordar','Tomar Café','Estudar']
+
+// function App() {
+//   return (
+//     <ul>{ tarefas.map((tarefa) => Task(tarefa))}</ul>
+//   );
+// }
+
+function App () {
   return (
-    <li key={value}>{value}</li>
-  );
-}
-
-const tarefas = ['Acordar','Tomar Café','Estudar']
-
-function App() {
-  return (
-    <ul>{ tarefas.map((tarefa) => Task(tarefa))}</ul>
-  );
+  <>
+  <Header />
+  <Content /> 
+  <Footer />
+  </>)
+  
 }
 
 export default App;
